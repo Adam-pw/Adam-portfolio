bfd4fabaa4c4b1ce23b057d18f715d1aed954bb7
Adam-pw <adampithewan@gmail.com>
2022-04-28 00:17:47 +0530

    Changes in Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,7 +7,10 @@ const Header = () => {
         <div className={styles.heading}>
           <div className={styles.main_p}>{"Hello !!"}</div>
           <div className={styles.main_p}>{"Myself Adam Pithenwala"}</div>
-          <div className={styles.main_p}>{"From Madhav Institute of Technology And Science 🏫💯"}</div>      
+          <div className={styles.main_p}>{"( From Madhav Institute of Technology And Science 🏫💯 )"}</div>      
+        </div>
+        <div className={styles.photo}>
+          <img className={styles.img} src="/images/img.jpeg" alt="HELLO" />
         </div>
       </div>
   );
