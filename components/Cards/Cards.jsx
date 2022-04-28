0b789f5265b4427f290f8d4f0c22a13fe80b119b
Adam-pw <adampithewan@gmail.com>
2022-04-28 16:37:28 +0530

    Added animation

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -2,43 +2,51 @@ import Link from 'next/link'
 import React from 'react'
 import styles from "./Cards.module.scss"
 
+import { motion } from "framer-motion"
+
 const Cards = () => {
     return (
-        <div className={styles.main}>
-            <div className={styles.card}>
+        <motion.div className={styles.main}>
+            <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }} className={styles.card}>
                 <div className={styles.circle}>
                     <img src="/images/Github.png" className={styles.photo}></img>
                 </div>
                 <div className={styles.content}>
                     Check out my Github
                     <Link href="https://github.com/Adam-pw" passHref>
-                    <button className={styles.button}>Github</button>
+                        <button className={styles.button}>Github</button>
                     </Link>
                 </div>
-            </div>
-            <div className={styles.card}>
+            </motion.div>
+            <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }} className={styles.card}>
                 <div className={styles.circle}>
                     <img src="/images/Linkedin.png" className={styles.photo}></img>
                 </div>
                 <div className={styles.content}>
                     Check out my Linkedin
                     <Link href="https://www.linkedin.com/in/adam-pithewan/" passHref>
-                    <button className={styles.button}>Linkedin</button>
+                        <button className={styles.button}>Linkedin</button>
                     </Link>
                 </div>
-            </div>
-            <div className={styles.card}>
+            </motion.div>
+            <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }} className={styles.card}>
                 <div className={styles.circle}>
                     <img src="/images/Instagram.png" className={styles.photo}></img>
                 </div>
                 <div className={styles.content}>
                     Check out my Instagram
                     <Link href="https://www.instagram.com/_._.adam._/" passHref>
-                    <button className={styles.button}>Instagram</button>
+                        <button className={styles.button}>Instagram</button>
                     </Link>
                 </div>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
