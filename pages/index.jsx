e38ec514a51fffdcb6c47e2d50c7cbb271aace3f
Adam-pw <adampithewan@gmail.com>
2022-04-30 14:19:26 +0530

    Title

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,9 +12,11 @@ export default function Home() {
   return (
     <>
       <Head>
+        <title>Adam Pithenwala</title>
         <meta charset="UTF-8" />
         <meta name="keywords" content="Adam, adam, Adam Pithenwala, adam pithenwala, Adam pithenwala" />
         <meta name="Description" content="About Adam Pithenwala" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       {/* <Navbar /> */}
       <Header />
