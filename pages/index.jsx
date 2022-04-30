47afc3c0ff49ee60770e7ef708158f80b1cf104d
Adam-pw <adampithewan@gmail.com>
2022-04-30 14:13:52 +0530

    SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,17 +6,24 @@ import Navbar from '../components/Navbar/Navbar'
 import Projectcards from '../components/Projectcards/Projectcards'
 import styles from '../styles/Home.module.scss'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="Adam, adam, Adam Pithenwala, adam pithenwala, Adam pithenwala" />
+        <meta name="Description" content="About Adam Pithenwala" />
+      </Head>
       {/* <Navbar /> */}
-      <Header/>
-      <Descrip/>
-      <Projectcards/>
-      <Projectcards/>
-      <Projectcards/>
-      <Cards/>
-      <Footer/>
+      <Header />
+      <Descrip />
+      <Projectcards />
+      <Projectcards />
+      <Projectcards />
+      <Cards />
+      <Footer />
     </>
   )
 }
