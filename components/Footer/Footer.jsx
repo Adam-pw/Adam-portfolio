768dc48f4c00be6c15cd30ba7c597391b5d7a1e5
Adam-pw <adampithewan@gmail.com>
2022-07-27 00:33:22 +0530

    fonts

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,22 +5,33 @@ import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className={styles.main}>
-      <div className={styles.main2}>
-        <div className={styles.last}>
-          <div className={styles.last1}><b>Adam</b></div>
-          <div className={styles.last2}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat earum quibusd</div>
-        </div>
-        <div className={styles.built}>
-          <div>© Made by Adam Pithenwala </div>
-          <div className={styles.icons}>
-            <div><FaInstagram /></div>
-            <div><FaLinkedin /></div>
-            <div><FaGithub /></div>
+    <>
+      <div className={styles.email}>
+        <div className={styles.emailsub}>
+          <div className={styles.head}><b>Subscribe for Notifications</b></div>
+          <div className={styles.text}>
+            <input type='email' className={styles.mail} placeholder="Email address"/>
+            <button type='button' className={styles.button}>Submit</button>
           </div>
         </div>
       </div>
-    </div>
+      <div className={styles.main}>
+        <div className={styles.main2}>
+          <div className={styles.last}>
+            <div className={styles.last1}><b>Adam</b></div>
+            <div className={styles.last2}>You can contact me via mail adampithewan@gmail.com or message me on any social media account</div>
+          </div>
+          <div className={styles.built}>
+            <div>© Made by Adam Pithenwala </div>
+            <div className={styles.icons}>
+              <div><FaInstagram /></div>
+              <div><FaLinkedin /></div>
+              <div><FaGithub /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
