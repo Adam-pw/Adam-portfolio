fcbd8fe4403207f73e2bad4008dd2c880fbed10f
Adam-pw <adampithewan@gmail.com>
2022-07-27 11:10:59 +0530

    responsive

--- components/Blogcom/Blogcom.jsx
+++ components/Blogcom/Blogcom.jsx
@@ -14,8 +14,8 @@ const Blogcom = () => {
                 <Link href='/blog'>
                     <div className={styles.box}>
                         <div className={styles.box1}>
-                            <div className={styles.box11}>PUBLISH DATE : 4 JANUARY 2020</div>
-                            <div className={styles.box12}><b>what did i learn</b></div>
+                            <div className={styles.box11}>PUBLISH DATE : 27 JULY 2022</div>
+                            <div className={styles.box12}><b>My Linux Customixation</b></div>
                         </div>
                         <div className={styles.box2}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, optio consequuntur! Eum eveniet vitae, dolor ducimus quisquam at pariatur. Officiis.
@@ -25,8 +25,8 @@ const Blogcom = () => {
                 <Link href='/blog'>
                     <div className={styles.box}>
                         <div className={styles.box1}>
-                            <div className={styles.box11}>PUBLISH DATE : 4 JANUARY 2020</div>
-                            <div className={styles.box12}><b>what did i learn</b></div>
+                            <div className={styles.box11}>PUBLISH DATE : 20 JULY 2022</div>
+                            <div className={styles.box12}><b>How Did I Created This Website</b></div>
                         </div>
                         <div className={styles.box2}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur facere cumque accusantium tempora ullam maxime rerum, omnis aut deleniti facilis.
@@ -36,8 +36,8 @@ const Blogcom = () => {
                 <Link href='/blog'>
                     <div className={styles.box}>
                         <div className={styles.box1}>
-                            <div className={styles.box11}>PUBLISH DATE : 4 JANUARY 2020</div>
-                            <div className={styles.box12}><b>what did i learn</b></div>
+                            <div className={styles.box11}>PUBLISH DATE : 16 JULY 2022</div>
+                            <div className={styles.box12}><b>Smart India Hackathon</b></div>
                         </div>
                         <div className={styles.box2}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere earum modi facilis necessitatibus. Libero cum ipsam illo, facere itaque eaque?
@@ -47,8 +47,8 @@ const Blogcom = () => {
                 <Link href='/blog'>
                     <div className={styles.box}>
                         <div className={styles.box1}>
-                            <div className={styles.box11}>PUBLISH DATE : 4 JANUARY 2020</div>
-                            <div className={styles.box12}><b>what did i learn</b></div>
+                            <div className={styles.box11}>PUBLISH DATE : 1 JULY 2020</div>
+                            <div className={styles.box12}><b>My Journey of Data Structures</b></div>
                         </div>
                         <div className={styles.box2}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere earum modi facilis necessitatibus. Libero cum ipsam illo, facere itaque eaque?
