47453f1d3d315264811300e3bbd9f78a1ee97889
Adam Pithenwala <adam@pop-os.localdomain>
2022-10-05 20:21:03 +0530

    design changes

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -1,57 +1,72 @@
-import Link from 'next/link'
-import React from 'react'
-import styles from "./Cards.module.scss"
+import Link from "next/link";
+import React from "react";
+import styles from "./Cards.module.scss";
 
-import { motion } from "framer-motion"
-import { Fade } from 'react-reveal'
+import { motion } from "framer-motion";
+import { Fade } from "react-reveal";
 
 const Cards = () => {
-    return (
+  return (
+    <>
+    <img src="/images/curve1.jpg" className={styles.img}></img>
+      <div className={styles.color}>
         <Fade left>
-            <motion.div className={styles.main}>
-                <motion.div
-                 id="contact"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }} className={styles.card}>
-                    <div className={styles.circle}>
-                        <img src="/images/Github.png" className={styles.photo}></img>
-                    </div>
-                    <div className={styles.content}>
-                        Check out my Github
-                        <Link href="https://github.com/Adam-pw" passHref>
-                            <button className={styles.button}>Github</button>
-                        </Link>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }} className={styles.card}>
-                    <div className={styles.circle}>
-                        <img src="/images/Linkedin.png" className={styles.photo}></img>
-                    </div>
-                    <div className={styles.content}>
-                        Check out my Linkedin
-                        <Link href="https://www.linkedin.com/in/adam-pithewan/" passHref>
-                            <button className={styles.button}>Linkedin</button>
-                        </Link>
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }} className={styles.card}>
-                    <div className={styles.circle}>
-                        <img src="/images/Instagram.png" className={styles.photo}></img>
-                    </div>
-                    <div className={styles.content}>
-                        Check out my Instagram
-                        <Link href="https://www.instagram.com/_._.adam._/" passHref>
-                            <button className={styles.button}>Instagram</button>
-                        </Link>
-                    </div>
-                </motion.div>
+          <motion.div className={styles.main}>
+            <motion.div
+              id="contact"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className={styles.card}
+            >
+              <div className={styles.circle}>
+                <img src="/images/Github.png" className={styles.photo}></img>
+              </div>
+              <div className={styles.content}>
+                {/* Check out my Github */}
+                <Link href="https://github.com/Adam-pw" passHref>
+                  <button className={styles.button}>Github</button>
+                </Link>
+              </div>
             </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className={styles.card}
+            >
+              <div className={styles.circle}>
+                <img src="/images/Linkedin.png" className={styles.photo}></img>
+              </div>
+              <div className={styles.content}>
+                {/* Check out my Linkedin */}
+                <Link
+                  href="https://www.linkedin.com/in/adam-pithewan/"
+                  passHref
+                >
+                  <button className={styles.button}>Linkedin</button>
+                </Link>
+              </div>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className={styles.card}
+            >
+              <div className={styles.circle}>
+                <img src="/images/Instagram.png" className={styles.photo}></img>
+              </div>
+              <div className={styles.content}>
+                {/* Check out my Instagram */}
+                <Link href="https://www.instagram.com/_._.adam._/" passHref>
+                  <button className={styles.button}>Instagram</button>
+                </Link>
+              </div>
+            </motion.div>
+          </motion.div>
         </Fade>
-    )
-}
+      </div>
+      <img src="/images/curve2.jpg" className={styles.img}></img>
+    </>
+  );
+};
 
-export default Cards
+export default Cards;
